647958f15a9a808e01c53bbedd01b259d707af32
Juyoung <syseoz@gmail.com>
2024-06-27 21:36:32 +0900

    feat token

--- src/main/java/com/project/shop/security/handler/LoginSuccessHandler.java
+++ src/main/java/com/project/shop/security/handler/LoginSuccessHandler.java
@@ -2,6 +2,7 @@ package com.project.shop.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.shop.customer.dto.AuthDTO;
+import com.project.shop.security.jwt.JWTProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.util.Map;
 
 @Slf4j
@@ -27,7 +29,16 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
         response.setContentType("application/json;charset=UTF-8");
 
         Map<String, Object> claims = authDTO.getClaims();
+        String accessToken = JWTProvider.generateToken(claims, 60*60*12);
+        String refreshToken = JWTProvider.generateRefreshToken();
+
+
+        claims.put("refreshToken", refreshToken);
+        claims.put("accessToken", accessToken);
+
 
         response.getWriter().write(objectMapper.writeValueAsString(claims));
     }
+
+
 }
--- src/main/java/com/project/shop/security/jwt/JWTProvider.java
+++ src/main/java/com/project/shop/security/jwt/JWTProvider.java
@@ -1,4 +1,74 @@
 package com.project.shop.security.jwt;
 
+import com.project.shop.global.exception.CustomJWTException;
+import com.project.shop.global.exception.enums.ExceptionCode;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.SecretKey;
+import java.io.UnsupportedEncodingException;
+import java.security.SecureRandom;
+import java.sql.Date;
+import java.time.ZonedDateTime;
+import java.util.Base64;
+import java.util.Map;
+
+@Slf4j
 public class JWTProvider {
+    private static String key = "034509324804235843098432144543676546435";
+    private static final int TOKEN_LENGTH = 64;
+    private static final SecureRandom secureRandom = new SecureRandom();
+    private static  final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
+
+    // 토큰 생성
+    public static String generateToken(Map<String,Object> valueMap, int min){
+        SecretKey key = null;
+
+        try {
+            key = Keys.hmacShaKeyFor(JWTProvider.key.getBytes("UTF-8"));
+        }catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+
+        String jwtStr = Jwts.builder()
+                .setHeaderParam("typ","JWT")
+                .setClaims(valueMap)
+                .setIssuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(min).toInstant()))
+                .signWith(key)
+                .compact();
+
+        return jwtStr;
+    }
+
+    public static Map<String,Object> validateToken(String token)   {
+        Map<String,Object> claims = null;
+
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(JWTProvider.key.getBytes("UTF-8")))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        }catch(MalformedJwtException malformedJwtException){
+            throw new CustomJWTException(ExceptionCode.MALFORM_TOKEN);
+        }catch(ExpiredJwtException expiredJwtException){
+            throw new CustomJWTException(ExceptionCode.EXPIRED_TOKEN);
+        }catch(InvalidClaimException invalidClaimException){
+            throw new CustomJWTException(ExceptionCode.INVALID_TOKEN);
+        }catch(JwtException jwtException){
+            throw new CustomJWTException(ExceptionCode.ERROR_TOKEN);
+        }catch(UnsupportedEncodingException e){
+            throw new RuntimeException(e.getMessage());
+        }
+        return claims;
+    }
+
+    public static String generateRefreshToken() {
+        byte[] randomBytes = new byte[TOKEN_LENGTH];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+
+    }
 }
